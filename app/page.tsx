6958055099a1708f06ca4c0466b794cd410e6417
Dmitry Drigota <drigota@gmail.com>
2024-06-14 17:09:48 +0100

    finalize basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Disclaimer from "@/components/Disclaimer";
 import { TaxForm } from "@/components/TaxForm";
+import TaxGraph from "@/components/TaxGraph";
+import TaxTable from "@/components/TaxTable";
 
 export default function Home() {
   return (
     <main className="container grid flex-grow grid-cols-1 grid-rows-3 gap-4 py-10 lg:grid-cols-2">
       <TaxForm />
+      <TaxGraph />
+      <TaxTable />
       <Disclaimer />
     </main>
   );
---
+++ components/TaxTable.tsx
@@ -0,0 +1,7 @@
+import { Card } from "./ui/card";
+
+const TaxTable = () => {
+  return <Card></Card>;
+};
+
+export default TaxTable;
